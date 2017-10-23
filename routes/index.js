3a2f7ad52373e20be12b976a72b701251a9ae8ef
alegabo93 <alegabo93@hotmail.com>
2017-10-23 16:38:59 -0500

    [fix] Add form example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {
+    name: 'Ale',
+    age: 24
+  },
+  {
+    name: 'Rosy',
+    age: 35
+  },
+  {
+    name: 'Paty',
+    age: 28
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    users: users
+  });
+});
+
+router.post('/addUser', function(req, res) {
+  users.push(req.body);
+  res.render('index', {
+    title: 'Express',
+    users: users
+  });
 });
 
 module.exports = router;
