892c0a5a9e38cb26e83871b8e72393d4085eedf8
alegabo93 <alegabo93@hotmail.com>
2017-10-25 17:53:06 -0500

    [feat] Add mongoose and an example of POST

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router(); // eslint-disable-line
 
+const Product = require('../models/products');
+
 /* GET products listing. */
 router.get('/product', (req, res) => {
   res
@@ -17,10 +19,26 @@ router.get('/product/:productId', (req, res) => {
 
 /* POST add product to list */
 router.post('/product', (req, res) => {
-  console.log(req.body);
-  res
-    .status(200)
-    .send({message: 'El producto se ha recibido'});
+  console.log('POST api/product', req.body);
+
+  const product = new Product();
+  product.name = req.body.name;
+  product.picture = req.body.picture;
+  product.price = req.body.price;
+  product.category = req.body.category;
+  product.description = req.body.description;
+
+  product.save((err, productStored) => {
+    if (err) {
+      res
+        .status(500)
+        .send({message: 'Error al guardar el producto'});
+    }
+
+    res
+      .status(200)
+      .send({product: productStored});
+  });
 });
 
 /* PUT change product by id. */
