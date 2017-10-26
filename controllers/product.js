dc9640cf62445e7a2ca79a68d5209847eb5e6cd8
alegabo93 <alegabo93@hotmail.com>
2017-10-26 11:46:51 -0500

    [feat] Add delete and put request and refactor code

---
+++ controllers/product.js
@@ -0,0 +1,127 @@
+const Product = require('../models/products');
+
+/* ==================================
+ * GET product by id
+ * ================================== */
+
+function getProduct(req, res) {
+  const {productId} = req.params;
+
+  Product.findById(productId, (err, product) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al realizar la petición'
+      });
+    }
+
+    if (!product) {
+      res.status(404).send({
+        message: 'El producto no existe'
+      });
+    }
+
+    res.status(200).send({product});
+  });
+}
+
+/* ==================================
+ * GET products listing.
+ * ================================== */
+
+function getProducts(req, res) {
+  Product.find({}, (err, products) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al realizar la petición'
+      });
+    }
+
+    if (!products) {
+      res.status(404).send({
+        message: 'No hay productos'
+      });
+    }
+
+    res.status(200).send({products});
+  });
+}
+
+/* ==================================
+ * POST product to list
+ * ================================== */
+
+function saveProduct(req, res) {
+  const {name, picture, price, category, description} = req.body;
+  const product = new Product({
+    name,
+    picture,
+    price,
+    category,
+    description
+  });
+
+  product.save((err, productStored) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al guardar el producto'
+      });
+    }
+
+    res.status(200).send({product: productStored});
+  });
+}
+
+/* ==================================
+ * PUT modified product by id
+ * ================================== */
+
+function updateProduct(req, res) {
+  const {productId} = req.params;
+  const update = req.body;
+
+  Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al modificar el producto'
+      });
+    }
+
+    res.status(200).send({product: productUpdated});
+  });
+}
+
+/* ==================================
+ * DELETE product by id
+ * ================================== */
+
+function deleteProduct(req, res) {
+  const {productId} = req.params;
+
+  Product.findById(productId, (err, product) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al borrar el producto'
+      });
+    }
+
+    product.remove(err => {
+      if (err) {
+        res.status(500).send({
+          message: 'Error al borrar el producto'
+        });
+      }
+
+      res.status(200).send({
+        message: 'El producto ha sido eliminado'
+      });
+    });
+  });
+}
+
+module.exports = {
+  getProducts,
+  getProduct,
+  saveProduct,
+  updateProduct,
+  deleteProduct
+};
--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,16 @@
 const express = require('express');
 const router = express.Router(); // eslint-disable-line
 
-const Product = require('../models/products');
+const productControllers = require('../controllers/product');
 
-/* GET products listing. */
-router.get('/product', (req, res) => {
-  res
-    .status(200)
-    .send({products: []});
-});
+/* ==================================
+ * Product Schema requests
+ * ================================== */
 
-/* GET product by id. */
-router.get('/product/:productId', (req, res) => {
-  res
-    .status(200)
-    .send({message: 'hola mundo'});
-});
-
-/* POST add product to list */
-router.post('/product', (req, res) => {
-  console.log('POST api/product', req.body);
-
-  const product = new Product();
-  product.name = req.body.name;
-  product.picture = req.body.picture;
-  product.price = req.body.price;
-  product.category = req.body.category;
-  product.description = req.body.description;
-
-  product.save((err, productStored) => {
-    if (err) {
-      res
-        .status(500)
-        .send({message: 'Error al guardar el producto'});
-    }
-
-    res
-      .status(200)
-      .send({product: productStored});
-  });
-});
-
-/* PUT change product by id. */
-router.put('/product/:productId', (req, res) => {
-  res
-    .status(200)
-    .send({message: 'hola mundo'});
-});
-
-router.delete('/product/:productId', (req, res) => {
-  res
-    .status(200)
-    .send({message: 'hola mundo'});
-});
+router.get('/product', productControllers.getProducts);
+router.get('/product/:productId', productControllers.getProduct);
+router.post('/product', productControllers.saveProduct);
+router.put('/product/:productId', productControllers.updateProduct);
+router.delete('/product/:productId', productControllers.deleteProduct);
 
 module.exports = router;
